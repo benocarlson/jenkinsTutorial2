349a4d4907c2c9a7dfa7464a7bb2f13fe96c9dfc
Ben Carlson <benocarlson@gmail.com>
2020-10-05 17:52:30 -0600

    Add a Main file

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        final String usage = "Usage: <cmd> <num1> <num2>";
+
+        if (args.length < 2) {
+            System.out.println(usage);
+        }
+
+        String function = args[0];
+        int num1 = Integer.parseInt(args[1]);
+        int num2 = 0;
+        if (args.length > 2) {
+            num2 = Integer.parseInt(args[2]);
+        }
+
+        Calculator calculator = new Calculator();
+
+        if (function.equals("add")) {
+            System.out.println(calculator.add(num1, num2));
+        } else if (function.equals("subtract")) {
+            System.out.println(calculator.subtract(num1, num2));
+        } else if (function.equals("multiply")) {
+            System.out.println(calculator.multiply(num1, num2));
+        } else if (function.equals("divide")) {
+            System.out.println(calculator.divide(num1, num2));
+        } else if (function.equals("binary")) {
+            System.out.println(calculator.intToBinaryNumber(num1));
+        } else if (function.equals("fibonacci")) {
+            System.out.println(calculator.fibonacciNumberFinder(num1));
+        }
+    }
+}
